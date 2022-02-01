9efafdc5c14b870c4b7fe38ba814d3e2103bafde
bendaryy <mohamedbendary1994@gmail.com>
2022-02-01 13:57:23 +0200

    cotton

--- EInvoicing/SubmitInvoices2.bat
+++ EInvoicing/SubmitInvoices2.bat
@@ -1,2 +1,2 @@
-D:\laragon\www\cottonlive\EInvoicing\publish/EInvoicingSigner.exe  D:\laragon\www\cottonlive\EInvoicing 18712477 "Egypt Trust Sealing CA"
+C:\laragon\www\cottonlive\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\cottonlive\EInvoicing 18712477 "Egypt Trust Sealing CA"
 PAUSE
